c2c11d8653cdb7c0a231b2193de39ee2072095b8
izabreb808 <izabreb808@student.polsl.pl>
2022-11-22 23:19:32 +0100

    Bottom menu

--- app/src/main/java/com/nszalas/timefulness/FragmentSignUp.kt
+++ app/src/main/java/com/nszalas/timefulness/FragmentSignUp.kt
@@ -56,5 +56,4 @@ class FragmentSignUp : Fragment() {
 
         return view
     }
-
 }
--- app/src/main/java/com/nszalas/timefulness/MainActivity.kt
+++ app/src/main/java/com/nszalas/timefulness/MainActivity.kt
@@ -2,6 +2,8 @@ package com.nszalas.timefulness
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -10,11 +12,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       /* val bottomMenuNavigation = findViewById<BottomNavigationView>(R.id.bottomMenuView)
-        val navController = findNavController(R.id.fragmentContainerView)
 
-        bottomMenuNavigation.setupWithNavController(navController)*/
+        val navController = this.findNavController(R.id.fragmentContainerView)
 
+        val bottomView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        navController.addOnDestinationChangedListener { _, nd: NavDestination, _ ->
+            if (nd.id == R.id.fragmentSignIn || nd.id == R.id.fragmentSignUp|| nd.id == R.id.fragmentStart) {
+                bottomView.visibility = View.GONE
+            } else {
+                bottomView.visibility = View.VISIBLE
+            }
+        }
 
     }
 }
