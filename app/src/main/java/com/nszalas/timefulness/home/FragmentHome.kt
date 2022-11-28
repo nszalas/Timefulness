fae00a065b89b557b35cbd7961fcba9b7870d8a1
izabreb808 <izabreb808@student.polsl.pl>
2022-11-28 23:17:02 +0100

    Model

--- app/src/main/java/com/nszalas/timefulness/home/FragmentHome.kt
+++ app/src/main/java/com/nszalas/timefulness/home/FragmentHome.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
 import com.nszalas.timefulness.R
 
 class FragmentHome : Fragment() {
@@ -18,6 +19,8 @@ class FragmentHome : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_your_profle, container, false)
+        val currentuser = FirebaseAuth.getInstance().currentUser!!.uid
+
         return view
     }
 
