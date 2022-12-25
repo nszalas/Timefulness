335b5d269279785a212fa2f7dd5481ca20eff41c
DamPolsl <damilat882@student.polsl.pl>
2022-12-25 12:46:07 +0100

    setup separate release and debug build types with singing files

--- app/src/main/java/com/nszalas/timefulness/ui/model/TaskWithCategoryUI.kt
+++ app/src/main/java/com/nszalas/timefulness/ui/model/TaskWithCategoryUI.kt
@@ -1,9 +1,11 @@
 package com.nszalas.timefulness.ui.model
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@Keep
 data class TaskWithCategoryUI(
     val task: TaskUI,
     val category: CategoryUI,
